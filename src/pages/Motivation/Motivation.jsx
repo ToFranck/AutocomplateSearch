11639e2f4218185703b56c538cc7833fe29b12c2
Franck Tolosano <tolosanofranck@hotmail.com>
2023-04-04 16:31:15 +0200

    tr

--- src/pages/Motivation/Motivation.jsx
+++ src/pages/Motivation/Motivation.jsx
@@ -7,6 +7,7 @@ export default function Motivation() {
     const [entreprise, setEntreprise] = useState('')
     const [recruteur, setRecruteur] = useState('')
     const [poste, setPoste] = useState('')
+    const [techno, setTechno] = useState('')
 
 
   return (
@@ -32,7 +33,26 @@ export default function Motivation() {
                 value={poste}
         />
 
+        <input type="text"
+                placeholder="Techno"
+                onChange={e => setTechno(e.target.value)}
+                value={techno}
+        />
+
         <p>{entreprise} {recruteur} {poste}</p>
+
+       <p> Cher/e { recruteur },
+J'ai le plaisir de vous présenter ma candidature pour le poste de { poste } dans votre entreprise. Avec mes années d'expérience dans le développement web et ma passion pour la programmation, je suis convaincu que je peux contribuer significativement à votre équipe.
+Au cours de mes études, j'ai travaillé sur une variété de projets qui ont exigé des compétences en développement web, en résolution de problèmes, gestion de projet et en travail d'équipe. J'ai également acquis un savoir dans l'utilisation de technologies telles que { techno }. 
+En tant que { poste }, je suis constamment à la recherche de nouvelles opportunités pour apprendre et améliorer mes compétences. Je suis très motivé par les enjeux et les projets complexes qui me permettent de développer mes compétences techniques et de résolution de problèmes. De plus, je suis un collaborateur passionné qui aime travailler en équipe et communiquer régulièrement avec les autres membres de l'équipe.
+Enfin, je tiens à souligner ma volonté de rejoindre votre entreprise en raison de sa réputation exceptionnelle, ainsi que son engagement envers l'excellence et l'innovation. Je suis convaincu que je serais un ajout précieux à votre équipe et je suis impatient d'avoir la possibilité de discuter avec vous en personne.
+Je vous remercie de votre temps et de votre considération. Veuillez trouver ci-joint mon CV pour plus d'informations sur mes compétences et mon expérience.
+Cordialement,
+Tolosano Franck</p>
+
+
+
+
 
     </>
   )
